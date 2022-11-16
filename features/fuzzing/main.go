3e98d0ad2b0244e888e0e15fc84fd2799bfcb1d9
sota_masuda <sota.masuda@the-lionsdata.com>
2022-11-16 18:18:24 +0900

    add reverse string function

--- features/fuzzing/main.go
+++ features/fuzzing/main.go
@@ -6,5 +6,22 @@ import (
 
 // Main is the entrypoint for the fuzzing package.
 func Main() {
-	fmt.Println("this is entrypoint for the fuzzing feature")
+	input := "Hello, World!"
+	rev := Reverse(input)
+	doubleRev := Reverse(rev)
+	fmt.Println("input:", input)
+	fmt.Println("reverse:", rev)
+	fmt.Println("double reverse:", doubleRev)
+}
+
+func Reverse(s string) string {
+	b := []byte(s)
+
+	// i start from 0, j start from the last index of the slice
+	// i and j are incremented and decremented respectively
+	// until i is equal to the half of the length of the slice
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i] // swap
+	}
+	return string(b)
 }
