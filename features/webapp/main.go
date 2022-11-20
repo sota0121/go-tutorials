592d61a291fcac1fed2990fa9b0079ee15cf6377
sota_masuda <sota.masuda@the-lionsdata.com>
2022-11-21 00:31:16 +0900

    add edit / save handler

--- features/webapp/main.go
+++ features/webapp/main.go
@@ -1,19 +1,14 @@
 package webapp
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 // Main is the entry point of the wiki application.
 func Main() {
-	title1st := "1st-page"
-	p1 := &Page{Title: title1st, Body: []byte("This is the 1st page.")}
-	p1.save()
-	p2, _ := loadPage(title1st)
-	fmt.Println(string(p2.Body))
-
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- features/webapp/view.go
+++ features/webapp/view.go
@@ -10,3 +10,27 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		// if the page does not exist, create a new page
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
