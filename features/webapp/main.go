98b768fd5d2adacbc490d8d503697b0354a27e20
sota_masuda <sota.masuda@the-lionsdata.com>
2022-11-20 11:44:41 +0900

    add Page struct and simple basic code

--- features/webapp/main.go
+++ features/webapp/main.go
@@ -2,8 +2,33 @@ package webapp
 
 import (
 	"fmt"
+	"os"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+// Main is the entry point of the wiki application.
 func Main() {
-	fmt.Println("This is the webapp feature")
+	title1st := "1st-page"
+	p1 := &Page{Title: title1st, Body: []byte("This is the 1st page.")}
+	p1.save()
+	p2, _ := loadPage(title1st)
+	fmt.Println(string(p2.Body))
 }
