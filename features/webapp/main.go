7156705fd77718712c18cc22f6f9aba56b03a904
sota_masuda <sota.masuda@the-lionsdata.com>
2022-11-20 11:54:22 +0900

    simple view handler

--- features/webapp/main.go
+++ features/webapp/main.go
@@ -2,27 +2,9 @@ package webapp
 
 import (
 	"fmt"
-	"os"
+	"log"
+	"net/http"
 )
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
 
 // Main is the entry point of the wiki application.
 func Main() {
@@ -31,4 +13,7 @@ func Main() {
 	p1.save()
 	p2, _ := loadPage(title1st)
 	fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
